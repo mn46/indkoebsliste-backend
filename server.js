2fdf8d4a662d32b43e5aa5b47fae17af38a79774
marta <m.nowak033@gmail.com>
2025-11-22 12:55:37 +0100

    Setup express server

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to indkoebsliste application." });
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
