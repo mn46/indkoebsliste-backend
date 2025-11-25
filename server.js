f9f4e7fb468a0314cbf05cc9537ad7ca97465a78
marta <m.nowak033@gmail.com>
2025-11-25 23:03:50 +0100

    Add verification of the jwt token

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 app.use(
@@ -17,15 +19,31 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const db = require("./app/models");
-db.sequelize.sync();
-
-// routes
-require("./app/routes/user.routes")(app);
-
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to indkoebsliste application." });
 });
+
+// verify jwt token
+app.use(cookieParser());
+
+const authenticateToken = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) return res.status(401).json("No token was provided.");
+
+  try {
+    req.user = jwt.verify(token, process.env.JWT_SECRET);
+    next();
+  } catch (err) {
+    return res.status(401).json("Invalid token.");
+  }
+};
+
+// protected routes
+app.use("/api", authenticateToken);
+require("./app/routes/user.routes")(app);
+
+const db = require("./app/models");
+db.sequelize.sync();
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
